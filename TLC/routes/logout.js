9458a7da4b0d05932dd62b184cf81c038b102f27
TLC_ADMIN <9606281+tlccpd@users.noreply.github.com>
2018-07-14 16:57:57 +0900

    Update logout.js

--- TLC/routes/logout.js
+++ TLC/routes/logout.js
@@ -8,27 +8,31 @@ module.exports =
 	// define the home page route
 	router.get('/', /* @callback */ function(req, res,id,pass) {
 		res.send('[LOGOUT]TLC');
-		res.render('index.jsp')
+		res.render('/logout/index.html')
 		 .get(/* @callback */ function(req, res){
                //여기에 동작 구현
                res.send('OK GET!');
          })
         .post(/* @callback */ function(req, res){
                u_id = req.param(id);
+	       u_pass = req.param(pass);
                //여기에 동작 구현
-               res.send('[id]'+id.toString());
+               res.send('[ ID ]'+u_id.toString());
+	       res.send('[ PW ]'+u_pass.toString());
          });
 	});
 	router.get('/return', /* @callback */ function(req, res,id,pass) {
 		res.send('[HOME]TLC');
-		res.render('../index.jsp')
+		res.render('/index.html')
 		 .get(/* @callback */ function(req, res){
                //여기에 동작 구현
                res.send('OK GET!');
          })
         .post(/* @callback */ function(req, res){
                u_id = req.param(id);
+	       u_pass = req.param(pass);
                //여기에 동작 구현
-               res.send('[id]'+id.toString());
+               res.send('[ ID ]'+u_id.toString());
+	       res.send('[ PW ]'+u_pass.toString());
          });
 	});
