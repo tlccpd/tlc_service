0d33bece620be60b9adc8287baeca843211cd7fc
TLC_ADMIN <9606281+tlccpd@users.noreply.github.com>
2018-07-14 15:01:43 +0900

    Update contact.js

--- TLC/routes/contact.js
+++ TLC/routes/contact.js
@@ -1,57 +1,35 @@
-var express = require('express');
-var router = express.Router();
-
+/*globals router */
 module.exports = 
-// middleware that is specific to this router
-	router.use(function (req, res, next) {
-		console.log('[ Date ] : ', Date.now());
-		next();
-	});
+ router.use(/* @callback */ function(req, res, next) {
+        // log each request to the console
+        console.log('[ Date ] : ', Date.now());
+        console.log(req.method, req.url);
+     
+       next();
+   });
 	// define the home page route
-	router.get('/', function(req, res) {
+	router.get('/', /* @callback */ function(req, res) {
 		res.send('TLC index');
-		res.render('index.jsp');
+		render.get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(function(req, res){
+               var id = req.param('tlc_admin');
+               //여기에 동작 구현
+               res.send("[ ID ]:"+ id.toString());
+         });
 	});
 	// define the about route
-	router.get('../admin', function(req, res) {
+	router.get('../admin', /* @callback */ function(req, res) {
 		res.send('ADMIN index');
-		res.render('index.jsp');
-	});
-
-
-
-/*
-function(app)
-{
-     app.get('/',function(req,res){
-        res.render('indexNew.jsp')
-     });     
-		
-     app.get('/admin',function(req,res){
-        res.render('indexNew.jsp');
+		render.get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(function(req, res){
+               var id = req.param('tlc_admin');
+               //여기에 동작 구현
+               res.send("[ ID ]:"+ id.toString());
+         });
      });
-	 app.get('/board',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/contact',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/convert',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/intro',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/login',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/logout',function(req,res){
-       res.render('about.jsp');
-     });
-	 app.get('/member',function(req,res){
-       res.render('about.jsp');
-     });
-	 app.get('/register',function(req,res){
-       res.render('about.jsp');
-     });
-	 */
