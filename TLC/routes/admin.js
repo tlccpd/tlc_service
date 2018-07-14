ebe0e6088789bcf9057acca2f4ef872c68037f0a
TLC_ADMIN <9606281+tlccpd@users.noreply.github.com>
2018-07-14 14:49:20 +0900

    Update admin.js

--- TLC/routes/admin.js
+++ TLC/routes/admin.js
@@ -1,27 +1,45 @@
+/*eslint-disable no-undef-expression */
 var express = require('express');
 var router = express.Router();
 
+
 module.exports = 
+	router.use(/* @callback */ function(req, res, next) {
+        // log each request to the console
+        console.log(req.method, req.url);
+        // continue doing what we were doing and go to the route
+       next();
+});
+
 // middleware that is specific to this router
-	router.use(function (req, res, next) {
+	router.use(/* @callback */ function (req, res, next) {
 		console.log('[ Date ] : ', Date.now());
 		next();
 	});
 	// define the home page route
-	router.get('/', function(req, res) {
+	router.get('/', /* @callback */ function(req, res) {
 		res.send('TLC index');
-		res.render('index.jsp');
+		res.render('/index.jsp')
+        .get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(function(req, res){
+               var id = req.param('tlc_admin');
+               //여기에 동작 구현
+               res.send("[ ID ]"+id.toString());
+         });
 	});
 	// define the about route
-	router.get('/login', function(req, res) {
+	router.get('/login', /* @callback */ function(req, res) {
 		res.send('ADMIN index');
 		res.render('login.jsp');
 	});
-	router.get('/logout', function(req, res) {
+	router.get('/logout', /* @callback */ function(req, res) {
 		res.send('ADMIN index');
 		res.render('logout.jsp');
 	});
-	router.get('/users', function(req, res) {
+	router.get('/users', /* @callback */ function(req, res) {
 		res.send('ADMIN index');
 		res.render('user.jsp');
 	});
