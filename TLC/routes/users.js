69dee7a44097edf4ed6ee2372a2912b411a52576
TLC_ADMIN <9606281+tlccpd@users.noreply.github.com>
2018-07-14 17:10:58 +0900

    Update users.js

--- TLC/routes/users.js
+++ TLC/routes/users.js
@@ -1,19 +1,38 @@
-var express = require('express');
-var router = express.Router();
-
+/*globals router */
 module.exports = 
 // middleware that is specific to this router
-	router.use(function (req, res, next) {
+	router.use(/* @callback */ function (req, res, next) {
 		console.log('[ Date ] : ', Date.now());
 		next();
 	});
 	// define the home page route
-	router.get('/', function(req, res) {
+	router.get('/', /* @callback */ function(req, res,id,pass) {
 		res.send('TLC index');
-		res.render('index.jsp');
+		res.render('/index.html')
+		.get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(function(req, res){
+               u_id = req.param(id);
+               u_pass = req.param(pass);
+               //여기에 동작 구현
+               res.send("[ ID ]"+id.toString());
+         });
 	});
 	// define the about route
-	router.get('/admin', function(req, res) {
+	router.get('/admin', /* @callback */ function(req, res,id,pass) {
 		res.send('ADMIN index');
-		res.render('../admin/index.jsp');
+		res.render('/admin/index.html')
+		.get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(function(req, res){
+               m_id = req.param(id);
+               m_pass = req.param(pass);
+               //여기에 동작 구현
+               res.send("[ ID ]"+m_id.toString());
+               res.send("[ PW ]"+m_pass.toString());
+         });
 	});
