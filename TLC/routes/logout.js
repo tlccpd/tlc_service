bd3b3f5fc529f1ac2cc425bf41bf94ba4ef869af
TLC_ADMIN <9606281+tlccpd@users.noreply.github.com>
2018-07-14 15:36:32 +0900

    Update logout.js

--- TLC/routes/logout.js
+++ TLC/routes/logout.js
@@ -1,58 +1,34 @@
-var express = require('express');
-var router = express.Router();
-
+/*globals router */
 module.exports = 
 // middleware that is specific to this router
-	router.use(function (req, res, next) {
+	router.use(/* @callback */ function (req, res, next) {
 		console.log('[ Date ] : ', Date.now());
 		next();
 	});
 	// define the home page route
-	router.get('/', function(req, res) {
+	router.get('/', /* @callback */ function(req, res,id,pass) {
 		res.send('[LOGOUT]TLC');
-		res.render('index.jsp');
+		res.render('index.jsp')
+		 .get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(/* @callback */ function(req, res){
+               u_id = req.param(id);
+               //여기에 동작 구현
+               res.send('[id]'+id.toString());
+         });
 	});
-	router.get('/return', function(req, res) {
+	router.get('/return', /* @callback */ function(req, res,id,pass) {
 		res.send('[HOME]TLC');
-		res.render('../index.jsp');
+		res.render('../index.jsp')
+		 .get(/* @callback */ function(req, res){
+               //여기에 동작 구현
+               res.send('OK GET!');
+         })
+        .post(/* @callback */ function(req, res){
+               u_id = req.param(id);
+               //여기에 동작 구현
+               res.send('[id]'+id.toString());
+         });
 	});
-
-
-
-
-
-/*
-function(app)
-{
-     app.get('/',function(req,res){
-        res.render('indexNew.jsp')
-     });     
-		
-     app.get('/admin',function(req,res){
-        res.render('indexNew.jsp');
-     });
-	 app.get('/board',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/contact',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/convert',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/intro',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/login',function(req,res){
-        res.render('about.jsp');
-     });
-	 app.get('/logout',function(req,res){
-       res.render('about.jsp');
-     });
-	 app.get('/member',function(req,res){
-       res.render('about.jsp');
-     });
-	 app.get('/register',function(req,res){
-       res.render('about.jsp');
-     });
-	 */
